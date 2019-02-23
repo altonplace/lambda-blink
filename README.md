539b70d4b5e541c62485a734a56900cbbee5f063
altonplace <maanderson4@gmail.com>
2019-02-23 16:59:17 -0500

    Updated to use slack for notifications vs. discord.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# lambda-blink
-Blink API interface for AWS Lambda
+# Lambda-Blink
+
+Basic Interface for toggling armed status for Blink Cameras.  Includes integration with Slack to communicate the status.
+
+### Blink variables
+To get your region and network, run this command substituting your username and password.  
+```
+curl -H "Host: prod.immedia-semi.com" -H "Content-Type: application/json" --data-binary '{ "password" : "your blink password", "client_specifier" : "iPhone 9.2 | 2.2 | 222", "email" : "your blink login/email" }' --compressed https://rest.prod.immedia-semi.com/login
+```
+
+### Slack variables
+
+Follow this guide to get a url:
+https://get.slack.help/hc/en-us/articles/115005265063-Incoming-WebHooks-for-Slack
+
+### CloudFormation
+Update the template.yaml.sample with the proper environment variables and rename to ```template.yaml```.
+
+Deploy to your AWS account.  Easiest way is to use the Pycharm AWSToolkit plugin.
+
--- main.py
+++ main.py
@@ -3,78 +3,78 @@ import os
 import json
 import re
 
-# Set Envionmental Variables
-region = os.environ['region']
-network = os.environ['network']
-discord_url = os.environ['discord_url']
-user = os.environ['user']
-passw = os.environ['passw']
+# Get/Set Environmental Variables
+region = os.environ['BlinkRegion']
+network = os.environ['BlinkNetwork']
+user = os.environ['BlinkUser']
+passw = os.environ['BlinkPass']
+slack_url = os.environ['SlackUrl']
 
 
-def send_message_discord(url, message):
-    payload = {'username': 'Blink',
-               'content': message
-               }
+def send_message_slack(url, message):
+    payload = {"text": message}
+
     data = json.dumps(payload)
     headers = {'Content-Type': 'application/json'}
+
     r = requests.post(url, data=data, headers=headers)
-    if r.status_code != 204:
+
+    if r.status_code != 200:
         print('post failed with error', r.status_code, 'because', r.reason)
     else:
         print('post success')
     return r
 
 
-def get_token():
-
-    url = 'https://rest.prod.immedia-semi.com/login'
-    headers = {'Host': "prod.immedia-semi.com",
-               'Content-Type': 'application/json'}
-
-    data = json.dumps({"email": user,
-                       "password": passw,
-                       "client_specifier": "iPhone 9.2 | 2.2 | 222"
-                       })
-
-    r = requests.post(url, headers=headers, data=data)
-
-    auth_token = re.search(r'{\"authtoken\":\"(.*?)\"', r.text).group(1)
-
-    return auth_token
-
-
 class Blink(object):
     def __init__(self):
-        self.auth_token = get_token()
+        self.auth_token = 'unset token'
+        self.get_token()
         self.region = region
         self.network = network
 
+    def get_token(self):
+
+        url = 'https://rest.prod.immedia-semi.com/login'
+        headers = {'Host': "prod.immedia-semi.com",
+                   'Content-Type': 'application/json'}
+
+        data = json.dumps({"email": user,
+                           "password": passw,
+                           "client_specifier": "iPhone 9.2 | 2.2 | 222"
+                           })
+
+        r = requests.post(url, headers=headers, data=data)
+
+        self.auth_token = re.search(r'{\"authtoken\":\"(.*?)\"', r.text).group(1)
+
     def get_status(self):
-        # Build URL
+
         url = 'https://rest.{}.immedia-semi.com/network/{}/syncmodules'.format(self.region, self.network)
-        print(url)
         r = requests.get(url, headers={'TOKEN_AUTH': self.auth_token})
         return r.text
 
     def alarm_set(self, state='arm'):
+
         state = state
+
         if state.upper() not in ('ARM', 'DISARM'):
             print("Invalid Input: alarm_set must be in either arm or disarm")
         else:
             if state.upper() == 'ARM':
                 message = 'House is armed'
+
             if state.upper() == 'DISARM':
-                message = 'House is disarmed'
-            # Build URL
+                message = 'House is unarmed'
+
             url = 'https://rest.{}.immedia-semi.com/network/{}/{}'.format(self.region, self.network, state)
-            print(url)
+
             r = requests.post(url, headers={'TOKEN_AUTH': self.auth_token})
-            send_message_discord(discord_url, message)
+
+            send_message_slack(slack_url, message)
 
             return r.text
 
-
-# Main
 
 Blink = Blink()
 
